b9d763dad2133d541d823b9a1a3a140bd0142d81
A-N1khil <nikhilanand1006@gmail.com>
2024-10-21 17:00:07 -0400

    Added detailed transaction

---
+++ src/main/java/spendreport/model/DetailedTransaction.java
@@ -0,0 +1,26 @@
+package spendreport.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * A transaction with additional detail of the zip code
+ * Zip Code will be uniformly selected between [01003, 02115, 78712]
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class DetailedTransaction {
+
+    private static final long serialVersionUID = 1L;
+
+    private long accountId;
+    private long timestamp;
+    private double amount;
+    private Integer zipCode;
+}
